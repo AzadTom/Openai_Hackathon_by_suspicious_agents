1aab36ae1eed01d6b9ad8c215dd6fd77e1fb65d9
azadtom <kumarazad2917@gmail.com>
2025-11-23 00:06:07 +0530

    added change to session key

--- Frontend/src/components/hooks/usegenerateMd5.ts
+++ Frontend/src/components/hooks/usegenerateMd5.ts
@@ -1,31 +1,32 @@
-'use client';
+"use client";
 
 import { usePathname } from "next/navigation";
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 import crypto from "crypto";
 
 export function generateUniqueString(length = 32) {
-    // Generate cryptographically strong random bytes
-    const bytes = crypto.randomBytes(length);
-  
-    // Convert to hex string (2 chars per byte)
-    return bytes.toString("hex").slice(0, length);
+  // Generate cryptographically strong random bytes
+  const bytes = crypto.randomBytes(length);
+
+  // Convert to hex string (2 chars per byte)
+  return bytes.toString("hex").slice(0, length);
 }
 
-export const usegenerateMd5 = ()=>{
+export const usegenerateMd5 = () => {
+  const pathname = usePathname();
+  const [session_key, setSessionKey] = useState("");
 
-    const pathname = usePathname();
-    const [session_key,setSessionKey] = useState("");
+  useEffect(() => {
+    const isKeyExist = localStorage.getItem("session_key");
+    if (isKeyExist) {
+      setSessionKey(isKeyExist);
+      return;
+    }
+    const session_key = generateUniqueString();
+    localStorage.setItem("session_key", session_key);
+    setSessionKey(session_key);
+    return;
+  }, [pathname]);
 
-    useEffect(()=>{
-     const isKeyExist = localStorage.getItem("session_key");
-     if(isKeyExist){
-        setSessionKey(isKeyExist);
-        return;
-     }
-     setSessionKey(generateUniqueString());
-     return;
-    },[pathname]);
-
-    return {session_key};
-} 
+  return { session_key };
+};
